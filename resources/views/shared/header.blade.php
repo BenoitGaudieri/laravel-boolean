53c0ff8d522ca7baa0c149d93c0eb928db3dcb72
BenoitGaudieri <benalek@gmail.com>
2020-06-11 17:32:22 +0200

    fix style

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Boolean</title>
-<link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <link rel="shortcut icon" href="https://www.boolean.careers/images/favicon/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
 </head>
 <body>
 
